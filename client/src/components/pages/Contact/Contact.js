f3a41857e635b2ac657b3729916e42baaa26f4cf
clintforster <clinte.forster@gmail.com>
2019-02-26 11:41:12 -0800

    portfolio push

--- client/src/components/pages/Contact/Contact.js
+++ client/src/components/pages/Contact/Contact.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import Title from "../../Title/Title.js";
+import { AwesomeButtonSocial } from "react-awesome-button";
+import { AwesomeButtonProgress } from "react-awesome-button";
+import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
 import "./Contact.css";
 
 class Contact extends Component {
@@ -6,7 +10,68 @@ class Contact extends Component {
     render() {
         return (
             <div>
+                <Title />
+                <div className="contianer pad">
+                    <div className="row">
+                        <div className="col-md module-border-wrap">
+                            <div className="module">
+                                <p>Follow Me on Linkedin!</p>
+                                <AwesomeButtonSocial
+                                    size="medium"
+                                    type="primary"
+                                    disabled="false"
+                                    fakePress="false"
+                                    ripple="true"
+                                    cssModule={AwesomeButtonStyles}
+                                    type="linkedin"
+                                    url="https://www.linkedin.com/in/clint-forster"
+                                >Linkedin</AwesomeButtonSocial>
+                            </div>
+                        </div>
+                        <div className="col-md module-border-wrap">
+                            <div className="module">
+                                <p>Take a copy of my résumé!</p>
 
+                                <AwesomeButtonProgress
+                                    size="medium"
+                                    type="primary"
+                                    disabled="false"
+                                    fakePress="false"
+                                    ripple="true"
+                                    cssModule={AwesomeButtonStyles}
+                                    action={(element, next) => {
+                                        setTimeout(() => {
+                                            next();
+                                        }, 600);
+                                    }}
+                                >Résumé</AwesomeButtonProgress>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md module-border-wrap">
+                            <div className="module">
+                                <p>Checkout my photography on Instagram!</p>
+                                <AwesomeButtonSocial
+                                    size="medium"
+                                    type="primary"
+                                    disabled="false"
+                                    fakePress="false"
+                                    ripple="true"
+                                    cssModule={AwesomeButtonStyles}
+                                    type="instagram"
+                                    url="https://www.instagram.com/clintforster/"
+                                    loadingLabel="hold on"
+                                >Instagram</AwesomeButtonSocial>
+                            </div>
+                        </div>
+                        <div className="col-md module-border-wrap">
+                            <div className="module">
+                                <p>Feel free to email me with any comments and questions!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
