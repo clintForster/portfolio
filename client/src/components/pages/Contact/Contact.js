1999a9a868883651a116d05eede4004d4e1c4698
clintforster <clinte.forster@gmail.com>
2019-02-26 15:05:06 -0800

    gradient borders in contact

--- client/src/components/pages/Contact/Contact.js
+++ client/src/components/pages/Contact/Contact.js
@@ -3,7 +3,7 @@ import Title from "../../Title/Title.js";
 import {
     AwesomeButton,
     AwesomeButtonSocial,
-  } from 'react-awesome-button';
+} from 'react-awesome-button';
 import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
 import "./Contact.css";
 
@@ -23,55 +23,69 @@ class Contact extends Component {
 
     render() {
         return (
-            <div>
-                <Title 
-                scrollValue={this.state.currentScrollHeight}
+            <div className="holder">
+                <Title
+                    scrollValue={this.state.currentScrollHeight}
                 />
                 <div className={this.state.currentScrollHeight < 80 ? ("container pad") : ("container pad margin")}>
                     <div className="row">
-                        <div className="col-md module-border-wrap">
+                        <div className="col-md module-border-wrap1">
                             <div className="module">
-                                <p>Follow Me on Linkedin!</p>
-                                <AwesomeButtonSocial
-                                    size="medium"
-                                    type="primary"
-                                    ripple="true"
-                                    cssModule={AwesomeButtonStyles}
-                                    type="linkedin"
-                                    href="https://www.linkedin.com/in/clint-forster"
-                                >Linkedin</AwesomeButtonSocial>
+                                {/* <p>Follow Me on Linkedin!</p> */}
+                                <div className="buttonDiv">
+                                    <AwesomeButtonSocial
+                                        size="medium"
+                                        type="primary"
+                                        ripple="true"
+                                        cssModule={AwesomeButtonStyles}
+                                        type="linkedin"
+                                        href="https://www.linkedin.com/in/clint-forster"
+                                    >Linkedin</AwesomeButtonSocial>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-md module-border-wrap">
+                        <div className="col-md module-border-wrap2">
                             <div className="module">
-                                <p>Take a copy of my résumé!</p>
-                                <AwesomeButton
-                                    size="medium"
-                                    type="primary"
-                                    ripple="true"
-                                    download="Resume.pdf"
-                                    cssModule={AwesomeButtonStyles}
-                                >Resume</AwesomeButton>
+                                {/* <p>Take a copy of my résumé!</p> */}
+                                <div className="buttonDiv" >
+                                    <AwesomeButton
+                                        size="medium"
+                                        type="primary"
+                                        ripple="true"
+                                        download="Resume.pdf"
+                                        cssModule={AwesomeButtonStyles}
+                                    >Resume</AwesomeButton>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md module-border-wrap">
+
+                        <div className="col-md module-border-wrap3">
                             <div className="module">
-                                <p>Checkout my photography on Instagram!</p>
-                                <AwesomeButtonSocial
-                                    size="medium"
-                                    type="primary"
-                                    ripple="true"
-                                    cssModule={AwesomeButtonStyles}
-                                    type="instagram"
-                                    href="https://www.instagram.com/clintforster/"
-                                >Instagram</AwesomeButtonSocial>
+                                {/* <p>Checkout my photography on Instagram!</p> */}
+                                <div className="buttonDiv">
+                                    <AwesomeButtonSocial
+                                        size="medium"
+                                        type="primary"
+                                        ripple="true"
+                                        cssModule={AwesomeButtonStyles}
+                                        type="instagram"
+                                        href="https://www.instagram.com/clintforster/"
+                                    >Instagram</AwesomeButtonSocial>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-md module-border-wrap">
+                        <div className="col-md module-border-wrap4">
                             <div className="module">
-                                <p>Feel free to email me with any comments and questions!</p>
+                                {/* <p>Feel free to email me with any comments and questions!</p> */}
+                                <div className="buttonDiv">
+                                    <AwesomeButton
+                                        size="medium"
+                                        type="primary"
+                                        ripple="true"
+                                        download="Resume.pdf"
+                                        cssModule={AwesomeButtonStyles}
+                                    >E-mail</AwesomeButton>
+                                </div>
                             </div>
                         </div>
                     </div>
