d88453bb5202bb376818930cdbef1a5cd9e6a68f
clintforster <clinte.forster@gmail.com>
2019-02-26 16:17:15 -0800

    wash

--- client/src/components/pages/Portfolio/Portfolio.js
+++ client/src/components/pages/Portfolio/Portfolio.js
@@ -1,12 +1,35 @@
 import React, { Component } from "react";
+import Title from "../../Title/Title.js";
 import "./Portfolio.css";
 
 class Portfolio extends Component {
 
+    state = {
+        currentScrollHeight: 0
+    }
+
+    componentDidMount() {
+        window.onscroll = () => {
+            this.setState({
+                currentScrollHeight: window.scrollY
+            });
+        }
+    }
+
     render() {
         return (
             <div>
-                
+                <Title
+                    scrollValue={this.state.currentScrollHeight}
+                />
+                <div className="row">
+                    <div className="col-md large">
+                        
+                    </div>
+                    <div className="col-md small">
+                        
+                    </div>
+                </div>
             </div>
         );
     }
